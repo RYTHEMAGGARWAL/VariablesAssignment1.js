37ff7c99c720f2e8da51a734a47d4ff79af42bcd
Rythem aggarwal <115411745+RYTHEMAGGARWAL@users.noreply.github.com>
2024-04-14 17:37:19 +0530

    Add files via upload

---
+++ problem2.js
@@ -0,0 +1,10 @@
+var name="Rythem Aggarwal"
+console.log("My Name is: ",name)
+
+
+var father_name="Sachin Aggarwal"
+console.log("My Father Name is: ",father_name)
+
+
+var mother_name="Sunita Aggarwal"
+console.log("My Mother Name is: ",mother_name)
---
+++ problem3.js
@@ -0,0 +1,4 @@
+var name="Rythem Aggarwal"
+var age=22
+console.log("Name: ",name)
+console.log("age: ",age)
